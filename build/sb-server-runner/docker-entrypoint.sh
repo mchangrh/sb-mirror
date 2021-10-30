286656966ac16697a1f9ee813b39f119b60da64e
Michael C <michael@mchang.name>
2021-10-29 22:43:16 -0400

    build with git clone while troubleshooting postgres

--- build/sb-server-runner/docker-entrypoint.sh
+++ build/sb-server-runner/docker-entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 echo "Licenced under the MIT Licence https://github.com/ajayyy/SponsorBlockServer" 
 
-curl -sLO https://github.com/ajayyy/SponsorBlockServer/archive/refs/heads/master.zip && unzip master.zip -q
-mv ./SponsorBlockServer-master/* ./SponsorBlockServer-master/.* /app/ 2>/dev/null
-rm -rf ./SponsorBlockServer-master
-
-mv /build/node_modules /app/node_modules
+git pull
 
 if [ -n "$DBINIT" ]; then echo '{"mode": "init-db-and-exit"}' > /app/config.json; fi
 npm start
