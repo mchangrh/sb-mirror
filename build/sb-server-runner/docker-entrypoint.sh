06c74d4715ff48e0e94960d2828f8fa1a8253877
Michael C <michael@mchang.name>
2023-06-23 21:02:31 -0400

    remove rsync, sb-runner, update containers

--- build/sb-server-runner/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Licenced under the MIT Licence https://github.com/ajayyy/SponsorBlockServer" 
-
-if [ -n "$DBINIT" ]; then echo '{"mode": "init-db-and-exit"}' > /app/config.json; fi
-npm start
-if [ -n "$DBINIT" ]; then mkdir -p /export && cp /app/databases/sponsorTimes.db /export/sponsorTimes.db; fi
